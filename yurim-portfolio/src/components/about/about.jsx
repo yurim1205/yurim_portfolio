888bc7e57ffee39bfe3113b91fe3cb183637f693
yurim1205 <eoulim3237@naver.com>
2025-03-12 19:43:07 +0900

    about페이지 작업

--- yurim-portfolio/src/components/about/about.jsx
+++ yurim-portfolio/src/components/about/about.jsx
@@ -9,9 +9,14 @@ const About = () => {
       className="h-screen bg-[#FFEDD2] flex flex-col items-center text-gray-800 px-4"
     >
       <p className="text-[48px] mb-8 font-thin font-abhaya underline decoration-[2px] pt-32">ABOUT ME</p>
-
+     
       <p className="text-2xl text-center max-w-3xl leading-8 pt-12">
-        안녕하세요! UX를 고려한 디자인을 우선시하며, 새로운 기술을 배우는 것을 좋아하는 프론트엔드 개발자 이유림입니다.
+        안녕하세요! 겅청을 좋아하는 이유림입니다.
+      </p>
+      
+      <p className="text-2xl text-center max-w-3xl leading-8 pt-8">
+        주도적으로 새로운 기술을 학습하는 것을 좋아하며, <br />
+        UX를 우선시하며 개발하는 프론트엔드 개발자로 성장하고 싶습니다.
       </p>
     </section>
   );
