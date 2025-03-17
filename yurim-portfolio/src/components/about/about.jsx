fa20462824c3708eb0ed6ac1b67a2f0636b5c113
yurim1205 <eoulim3237@naver.com>
2025-03-17 23:38:20 +0900

    style: css 수정

--- yurim-portfolio/src/components/about/about.jsx
+++ yurim-portfolio/src/components/about/about.jsx
@@ -9,13 +9,12 @@ import EmailIcon from "../../assets/email.png";
 const About = () => {
   const infoItems = [
     // 수정하기 편하게 아이콘 크기 따로 지정함
-    { id: 1, icon: PersonIcon, title: "이름", body: "이유림", size: "h-20 w-20" },
-    { id: 2, icon: CalenderIcon, title: "생년월일", body: "2000.12.05", size: "h-20 w-20" },
-    { id: 3, icon: GraduationIcon, title: "전공", body: "컴퓨터소프트웨어공학", size: "h-20 w-20" },
+    { id: 1, icon: PersonIcon, body: "이유림", size: "h-20 w-20" },
+    { id: 2, icon: CalenderIcon, body: "2000.12.05", size: "h-20 w-20" },
+    { id: 3, icon: GraduationIcon, body: "컴퓨터소프트웨어공학", size: "h-20 w-20" },
     {
       id: 4,
       icon: EmailIcon,
-      title: "이메일",
       body: (
         <>
           yuurriimm@gmail.com
@@ -33,15 +32,15 @@ const About = () => {
       className="w-full min-h-screen mx-auto bg-[#FFEDD2] flex flex-col items-center justify-center px-4 py-32"
     >
       <div className="max-w-6xl w-full px-4 mb-32">
-        <p className="mb-24 text-[96px] sm:text-[64px] text-center font-thin font-abhaya underline decoration-[2px]">
+        <p className="mb-24 text-[96px] sm:text-[64px] text-center font-thin font-abhaya decoration-[2px]">
           ABOUT ME
         </p>
 
-        <p className="text-3xl sm:text-2xl text-center max-w-3xl mx-auto leading-8 text-main font-sans">
+        <p className="sm:text-3xl text-center max-w-3xl mx-auto leading-8 text-main font-sans">
           안녕하세요! 경청을 좋아하는 이유림입니다.
         </p>
 
-        <p className="text-3xl sm:text-2xl text-center max-w-4xl mx-auto leading-snug mt-4 text-main font-sans">
+        <p className="text-3xl text-center max-w-4xl mx-auto leading-snug mt-5 text-main font-sans">
           <span className="font-bold">주도적</span>으로 새로운 기술을 학습하는 것을 좋아하며, <br />
           <span className="font-bold">UX를 우선시하며 개발하는</span> 프론트엔드 개발자로
           <span className="font-bold"> 성장</span>하고 싶습니다.
@@ -50,9 +49,8 @@ const About = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 pt-36">
           {infoItems.map((item) => (
             <div className="flex flex-col items-center" key={item.id}>
-              <img src={item.icon} alt={`${item.title} 아이콘`} className="h-20 w-20" />
-              <p className="text-2xl mt-4 font-bold text-main">{item.title}</p>
-              <p className="text-xl mt-4 text-main text-center">{item.body}</p>
+              <img src={item.icon} alt={`${item.title} 아이콘`} className="mt-10 h-20 w-20" />
+              <p className="text-xl mt-8 text-main text-center">{item.body}</p>
             </div>
           ))}
         </div>
