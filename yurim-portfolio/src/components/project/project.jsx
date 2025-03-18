1b9de8729255d4e83652caaf5620e331ca229e5d
yurim1205 <eoulim3237@naver.com>
2025-03-18 22:08:33 +0900

    feat:기본 설정

---
+++ yurim-portfolio/src/components/project/project.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Project = () => {
+    return (
+        <section
+            id="project"
+            className="w-full min-h-screen py-20 mx-auto flex flex-col justify-start items-center relative bg-main overflow-hidden"
+        >
+
+
+        </section>
+    );
+};
+
+export default Project;
