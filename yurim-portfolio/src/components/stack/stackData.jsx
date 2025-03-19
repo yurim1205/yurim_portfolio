4592b73369229423ce366ac60e88d957b44a37dd
yurim1205 <eoulim3237@naver.com>
2025-03-19 22:42:46 +0900

    feat: 데이터 내용 수정

--- yurim-portfolio/src/components/stack/stackData.jsx
+++ yurim-portfolio/src/components/stack/stackData.jsx
@@ -14,19 +14,19 @@ import TailwindIcon from "../../assets/stackIcon/tailwind-css.png";
 
 export const FrontendIcons = [
     { id: 1, src: HtmlIcon, alt: "Html", title: "HTML5", description: "웹 표준을 준수하며, 반응형 웹을 구축하여 최적화된 사용자 경험을 제공합니다." },
-    { id: 2, src: JsIcon, alt: "JavaScript", title: "JavaScript", description: "" },
-    { id: 3, src: CssIcon, alt: "CSS Icon", title: "CSS3", description: "" },
-    { id: 4, src: TsIcon, alt: "TypeScript Icon", title: "TypeScript", description: "" },
-    { id: 5, src: ReactIcon, alt: "React Icon", title: "React", description: "" },
-    { id: 6, src: StyledComponentsIcon, alt: "StyledComponents Icon", title: "Styled Components", description: "" },
-    { id: 7, src: TailwindIcon, alt: "Tailwind Icon", title: "Tailwind CSS", description: "" },
+    { id: 2, src: JsIcon, alt: "JavaScript", title: "JavaScript", description: "웹 애플리케이션 개발에 필수적인 프로그래밍 언어로, 동적인 기능 구현이 가능합니다." },
+    { id: 3, src: CssIcon, alt: "CSS Icon", title: "CSS3", description: "flexbox, grid를 사용한 레이아웃을 만들고 미디어 쿼리를 통해 반응형 웹을 제작합니다." },
+    { id: 4, src: TsIcon, alt: "TypeScript Icon", title: "TypeScript", description: "인터페이스와 제네릭 등에 대한 기본적인 원리를 이해합니다." },
+    { id: 5, src: ReactIcon, alt: "React Icon", title: "React", description: "컴포넌트를 재사용하고 상태 관리를 통해 효율적으로 UI를 구축할 수 있습니다." },
+    { id: 6, src: StyledComponentsIcon, alt: "StyledComponents Icon", title: "Styled Components", description: "CSS-in-JS 방식을 통해 컴포넌트 단위로 스타일을 정의합니다." },
+    { id: 7, src: TailwindIcon, alt: "Tailwind Icon", title: "Tailwind CSS", description: "재사용 가능한 유틸리티 클래스를 통해 일관된 디자인과 효율적인 개발을 할 수 있습니다." },
 ];
 
 export const ToolsIcons = [
-    { id: 7, src: AndroidStudioIcon, alt: "AndroidStudio Icon", title: "Android Studio", description: "" },
-    { id: 8, src: VsCodeIcon, alt: "VsCode Icon", title: "VSCode", description: "" },
-    { id: 9, src: PostmanIcon, alt: "Postman Icon", title: "Postman", description: "" },
-    { id: 10, src: FigmaIcon, alt: "Figma Icon", title: "Figma", description: "" },
-    { id: 11, src: GitIcon, alt: "Git Icon", title: "Git", description: "" },
-    { id: 12, src: GitLabIcon, alt: "GitLab Icon", title: "GitLab", description: "" }
+    { id: 7, src: AndroidStudioIcon, alt: "AndroidStudio Icon", title: "Android Studio", description: "해당 툴을 통해 플러터 앱을 개발한 경험이 있습니다." },
+    { id: 8, src: VsCodeIcon, alt: "VsCode Icon", title: "VSCode", description: "다양한 확장 프로그램을 사용하여 효율적으로 개발 가능합니다." },
+    { id: 9, src: PostmanIcon, alt: "Postman Icon", title: "Postman", description: "REST API 테스트 및 공유 시 사용하는 툴입니다." },
+    { id: 10, src: FigmaIcon, alt: "Figma Icon", title: "Figma", description: "UI/UX 디자인 및 프로토타입을 제작할 수 있습니다." },
+    { id: 11, src: GitIcon, alt: "Git Icon", title: "Git", description: "Github와 함께 사용하며, 로컬 & 원격 저장소를 이용한 버전 관리 및 협업이 가능합니다." },
+    { id: 12, src: GitLabIcon, alt: "GitLab Icon", title: "GitLab", description: "GitLab을 통해 프로젝트를 관리하며 협업한 경험이 있습니다." }
 ];
