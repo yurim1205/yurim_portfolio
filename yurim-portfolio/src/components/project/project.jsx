473e0271401e3992bca884c31b46c0e824305dcc
yurim1205 <eoulim3237@naver.com>
2025-03-21 22:50:50 +0900

    feat: 프로젝트 이미지 추가, 파일 분리

--- yurim-portfolio/src/components/project/project.jsx
+++ yurim-portfolio/src/components/project/project.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import projectImg1 from "../../assets/projectImg/ABOUT.png";
 
 const Project = () => {
     return (
@@ -6,8 +7,30 @@ const Project = () => {
             id="project"
             className="w-full min-h-screen py-20 mx-auto flex flex-col justify-start items-center relative bg-main overflow-hidden"
         >
+            <p className="mt-8 text-[96px] sm:text-[64px] text-center font-thin font-abhaya decoration-[2px] text-[#FFEDD2]">
+                PROJECT
+            </p>
 
+            <div className="mt-4 w-[1200px] h-[2px] bg-text">
+            </div>
 
+            <div className="mt-4 w-[1200px] h-[700px] flex items-center justify-center">
+                {/* 프로젝트 대표 이미지 */}
+                <div className="w-1/2 h-full flex items-center justify-center">
+                    <div className="w-[80%] h-[80%] rounded-lg flex items-center justify-center overflow-hidden">
+                        <img
+                            src={projectImg1} className="w-full h-full object-contain"
+                        />
+                    </div>
+                </div>
+
+                {/* 가운데 선 */}
+                <div className="w-[2px] h-[80%] bg-[#FFEDD2]"></div>
+
+                <div className="w-1/2 h-full flex flex-col justify-center px-4">
+
+                </div>
+            </div>
         </section>
     );
 };
---
+++ yurim-portfolio/src/components/project/projectData.jsx
@@ -0,0 +1,20 @@
+const projectData = [
+    {
+        id: 1,
+        title: "개인 포트폴리오 사이트",
+        problem: "",
+        description: "",
+        stack: "",
+        link: ""
+    },
+    {
+        id: 2,
+        title: "트립텔러 (6인 프로젝트)",
+        problem: "",
+        description: "",
+        stack: "",
+        link: ""
+    }
+];
+
+export default projectData;
