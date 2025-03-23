1bb1a3bc254ab9ddfe3f1e75f6503355f65125f2
yurim1205 <eoulim3237@naver.com>
2025-03-23 21:48:49 +0900

    feat: css 설정 추가, 경로 추가

--- yurim-portfolio/tailwind.config.js
+++ yurim-portfolio/tailwind.config.js
@@ -5,7 +5,8 @@ export default {
     "./src/components/common/header.jsx",
     "./src/components/about/about.jsx",
     "./src/components/stack/stackList.jsx",
-    "./src/components/project/project.jsx"
+    "./src/components/project/project.jsx",
+    "./src/components/project/projectItem.jsx"
   ],
   theme: {
     extend: {
@@ -20,6 +21,10 @@ export default {
       },
       margin: {
         '96': '24rem', 
+      },
+      boxShadow: {
+        '3xl': '0 15px 30px rgba(0, 0, 0, 0.1)',
+        '4xl': '0 20px 40px rgba(0, 0, 0, 0.1)',
       },
     },
   },
