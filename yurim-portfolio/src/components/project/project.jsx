fbe8693d4e1b7b457794b481fc117db0a8ee214f
yurim1205 <eoulim3237@naver.com>
2025-03-25 23:03:15 +0900

    feat: footer에 이미지 추가

--- yurim-portfolio/src/components/project/project.jsx
+++ yurim-portfolio/src/components/project/project.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { projectData } from "./projectData";
 import ProjectItem from "./projectItem";
+import githubIcon from "../../assets/footerIcon/github.png";
+import velogIcon from "../../assets/footerIcon/velog.png";
 
 const Project = () => {
     return (
@@ -22,22 +24,23 @@ const Project = () => {
                 </div>
             </section>
 
-            <footer className="w-full bg-text h-[150px] flex items-center justify-center text-black">
+            <footer className="w-full bg-text h-[150px] flex items-center justify-center text-main">
                 <div className="container mx-auto flex justify-between w-full items-center">
                     <div className="flex space-x-4">
                         <a href="https://github.com/yurim1205" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-github text-xl"></i>
+                            <img src={githubIcon} alt="github" className="w-10 h-10" />
                         </a>
 
                         <a href="https://velog.io/@yurimi/posts" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-rss text-xl"></i>
+                            <img src={velogIcon} alt="velog" className="w-10 h-10" />
                         </a>
                     </div>
                     <p className="text-sm text-right">© 2025 LeeYurim. All Rights Reserved.</p>
                 </div>
             </footer>
+
         </div>
     );
-};
+}
 
 export default Project;
