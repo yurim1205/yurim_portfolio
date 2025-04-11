85eb54cd65446a5e7914e0f149852545d656a6f8
yurim1205 <eoulim3237@naver.com>
2025-04-11 17:00:25 +0900

    feat: 탑 버튼 수정

--- yurim-portfolio/src/App.jsx
+++ yurim-portfolio/src/App.jsx
@@ -99,7 +99,7 @@ const App = () => {
       {showButton && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-5 right-5 bg-white p-2 rounded-full shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-xl"
+          className="fixed bottom-5 right-5 bg-white p-2 rounded-full shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110 hover:shadow-xl z-50"
         >
           <img src={buttonIcon} className="w-10 h-10" />
         </button>
