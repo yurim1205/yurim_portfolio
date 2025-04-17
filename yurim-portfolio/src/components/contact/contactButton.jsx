edd86a9ffe5d4b2171ec281f63146b7bdcad9cec
yurim1205 <eoulim3237@naver.com>
2025-04-17 13:33:54 +0900

    feat: 버튼 수정

--- yurim-portfolio/src/components/contact/contactButton.jsx
+++ yurim-portfolio/src/components/contact/contactButton.jsx
@@ -4,7 +4,7 @@ function ContactButton({ href, text }) {
     return (
         <a
             href={href}
-            className="mt-8 rounded-full bg-white px-5 py-3 h-12 w-48 text-center
+            className="mt-8 rounded-full bg-white px-5 py-3 h-12 w-36 text-center
          text-main text-bold shadow-md hover:bg-text hover:scale-115 transition-transform 
          duration-500"
             target="_blank"
--- yurim-portfolio/src/components/contact/contactData.js
+++ yurim-portfolio/src/components/contact/contactData.js
@@ -2,5 +2,5 @@ export const CONTACT_LINKS = [
     {href:'https://velog.io/@yurimi/', text: 'Velog'},
     {href:'https://github.com/yurim1205', text: 'Github'},
     {href:'https://github.com', text: 'Resume'},
-   {href:'https://velog.io/', text: 'Linked In'}
+  // {href:'https://velog.io/', text: 'Linked In'}
 ];
