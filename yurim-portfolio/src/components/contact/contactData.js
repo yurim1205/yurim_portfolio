6cd7e2a24ca0377ddb28a26c5a982c4df5fe303b
yurim1205 <eoulim3237@naver.com>
2025-04-20 19:06:28 +0900

    feat: 데이터 추가

--- yurim-portfolio/src/components/contact/contactData.js
+++ yurim-portfolio/src/components/contact/contactData.js
@@ -1,6 +1,6 @@
 export const CONTACT_LINKS = [
     {href:'https://velog.io/@yurimi/', text: 'Velog'},
     {href:'https://github.com/yurim1205', text: 'Github'},
-    {href:'https://github.com', text: 'Resume'},
+    {href:'https://pitch-mint-e9e.notion.site/Yurim-Lee-bdd470cb1ab34aa3bb5564287402e0ad', text: 'Resume'},
   // {href:'https://velog.io/', text: 'Linked In'}
 ];
